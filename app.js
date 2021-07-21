3afb69927553a636eac6b5876ce1fa0c826083f9
Neel526 <63090511+Neel526@users.noreply.github.com>
2021-07-21 16:16:16 +0530

    fix(error message)

--- app.js
+++ app.js
@@ -6,19 +6,32 @@ var result = document.querySelector("#result");
 var btnNext = document.querySelector("#btnNext");
 
 var cashAmt = document.querySelector(".cashAmt")
+var err = document.querySelector(".err");
+var err1 = document.querySelector(".err1");
+
 
 function showCashAmt(){
-    btnNext.style.display = "none";
-    cashAmt.style.display = "block"
-
+    if(Number(billAmount.value) > 0){
+        btnNext.style.display = "none";
+        cashAmt.style.display = "block"
+        err.style.display = "none"
+    }else{
+        err.innerHTML = "Incorrect bill amount"
+    }
 }
 
 btnNext.addEventListener("click", showCashAmt)
 
 function callback(){
-    var res = cashAmount.value - billAmount.value;
-    console.log(res)
-    result.innerHTML = res + " Rs to give as change to Customer";
+    if(Number(billAmount.value) > 0 && Number(cashAmount.value) > 0){
+        var res = cashAmount.value - billAmount.value;
+        console.log(res)
+        result.innerHTML = res + " Rs to give as change to Customer";
+        err1.style.display = "none"
+    } else{
+        err1.style.display = "block"
+        err1.innerHTML = "Incorrect bill amount"
+    }
 }
 
 btnCheck.addEventListener("click", callback)
